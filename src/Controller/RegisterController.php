f071be3652667b5fcc5cbef124d294c9d42be679
Anthony André <anthony-andre@live.fr>
2022-02-18 09:40:22 +0100

    retour en arriere

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -7,15 +7,32 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User; 
 use app\form\RegisterType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 
 class RegisterController extends AbstractController
 {
+
+    public function __construct(private ManagerRegistry $doctrine) {}
+
     #[Route('/inscription', name: 'register')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
 
         $user = new User(); 
         $form =$this->createForm(RegisterType::class, $user); 
+
+        $form->handleRequest($request); 
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData(); 
+
+            $doctrine = $this->doctrine->getManager(); 
+            $doctrine->persist($user); 
+            $doctrine->flush(); 
+
+        }
 
         return $this->render('register/index.html.twig', [
             'form'=> $form->createView()
